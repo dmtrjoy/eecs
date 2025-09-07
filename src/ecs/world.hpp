d0f826b618062d2c1d71b03f485c45abd44c4e81
dmtrjoy <137457240+dmtrjoy@users.noreply.github.com>
2025-09-07 17:42:03 -0400

    Add flexible container for any type (#8)

--- src/ecs/world.hpp
+++ src/ecs/world.hpp
@@ -1,13 +1,13 @@
 #ifndef EECS_WORLD_HPP
 #define EECS_WORLD_HPP
 
-#include <any>
 #include <stdexcept>
 #include <tuple>
 #include <typeindex>
 #include <unordered_map>
 #include <utility>
 
+#include "any.hpp"
 #include "entity.hpp"
 #include "sparse_set.hpp"
 
@@ -17,36 +17,62 @@ namespace eecs {
 /// components, and resources.
 class world {
 public:
-    /// Creates a new entity.
+    /// Creates a new `::entity`.
+    ///
+    /// \return A new `::entity` identifier.
     entity create_entity() noexcept;
 
-    /// Adds a component to an entity.
+    /// Adds a component to an ::entity.
+    ///
+    /// \tparam Component The type of component to add.
+    /// \param entity The ::entity to add the component to.
+    /// \param component The component to add.
     template <typename Component>
     void add_component(entity entity, const Component& component);
 
-    /// Adds a resource.
+    /// Adds a resource to this ::world. The resource is constructed in place.
+    ///
+    /// Resources are unique data types managed by the ::world. They are
+    /// similar to singletons in that only one instance can exist at any time;
+    /// however, resources do not have global state. Their lifetimes are tied
+    /// to the ::world that owns them.
+    ///
+    /// \tparam Resource The type of resource to add.
+    /// \tparam Args Parameter pack for constructor argument types.
+    /// \param args Arguments forwarded to the resource constructor.
     template <typename Resource, typename... Args>
     void add_resource(Args&&... args);
 
-    /// Clears all entities.
+    /// Clears all entities from this ::world.
     void clear_entities();
 
-    /// Retrieves this world's components collection of a given type.
+    /// Retrieves this ::world's component collection of the given type.
+    ///
+    /// \tparam Component The type of component collection to retrieve.
+    /// \return A reference to the component collection.
     template <typename Component>
     sparse_set<Component>& components();
 
-    /// Retrives a resource.
+    /// Retrives a resource from this ::world.
+    ///
+    /// \tparam Resource The type of resource to retrieve.
+    /// \return A reference to the resource.
+    /// \throws std::out_of_range if the resource is not found.
     template <typename Resource>
     Resource& resource();
 
-    /// Calls a function on each entity with the given types of components.
+    /// Calls a function on each ::entity with the given types of components.
+    ///
+    /// \tparam Components The types of components that the entities must have.
+    /// \tparam Fn The type of function to call.
+    /// \param fn The function to call for each matching ::entity.
     template <typename... Components, typename Fn>
     void view(Fn&& fn);
 
 private:
     entity m_next_entity = 0;
-    std::unordered_map<std::type_index, std::any> m_components;
-    std::unordered_map<std::type_index, std::any> m_resources;
+    std::unordered_map<std::type_index, any> m_components;
+    std::unordered_map<std::type_index, any> m_resources;
 };
 
 inline entity world::create_entity() noexcept { return m_next_entity++; }
@@ -62,7 +88,10 @@ template <typename Resource, typename... Args>
 void world::add_resource(Args&&... args)
 {
     const auto type_id { std::type_index(typeid(Resource)) };
-    m_resources[type_id] = Resource { std::forward<Args>(args)... };
+    m_resources.emplace(std::piecewise_construct,
+        std::forward_as_tuple(type_id),
+        std::forward_as_tuple(
+            std::in_place_type_t<Resource> {}, std::forward<Args>(args)...));
 }
 
 inline void world::clear_entities()
@@ -78,10 +107,15 @@ sparse_set<Component>& world::components()
     auto it { m_components.find(type_id) };
 
     if (it == m_components.end()) {
-        it = m_components.emplace(type_id, sparse_set<Component> {}).first;
+        it = m_components
+                 .emplace(std::piecewise_construct,
+                     std::forward_as_tuple(type_id),
+                     std::forward_as_tuple(
+                         std::in_place_type_t<sparse_set<Component>> {}))
+                 .first;
     }
 
-    return std::any_cast<sparse_set<Component>&>(it->second);
+    return any_cast<sparse_set<Component>>(it->second);
 }
 
 template <typename Resource>
@@ -94,7 +128,7 @@ Resource& world::resource()
         throw std::out_of_range("Resource not found");
     }
 
-    return std::any_cast<Resource&>(it->second);
+    return any_cast<Resource>(it->second);
 }
 
 template <typename... Components, typename Fn>
---
+++ tests/any.t.cpp
@@ -0,0 +1,35 @@
+#include "any.hpp"
+
+#include "gtest/gtest.h"
+
+#include <typeindex>
+
+namespace eecs::test {
+
+TEST(AnyTest, DefaultConstructor)
+{
+    // GIVEN
+    // ...
+
+    // WHEN
+    const any any {};
+
+    // THEN
+    EXPECT_FALSE(any.has_value());
+    EXPECT_EQ(any.type(), std::type_index(typeid(void)));
+}
+
+TEST(AnyTest, CopyConstructor)
+{
+    // GIVEN
+    const any any1 { 100.0 };
+
+    // WHEN
+    // NOLINTNEXTLINE(performance-unnecessary-copy-initialization)
+    const any any2 { any1 };
+
+    // THEN
+    EXPECT_EQ(any1, any2);
+}
+
+} // namespace eecs::test
