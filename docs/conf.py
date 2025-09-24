1bef31efae9d7f42809cac918384eacff12bbc37
Dimitri Joy <dmtrjoy@gmail.com>
2025-09-24 08:41:54 -0400

    Set up documentation with Sphinx + Breathe

---
+++ docs/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'eecs'
+copyright = '2025, Dimitri'
+author = 'Dimitri'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path('./.venv/lib/python3.13/site-packages').resolve()))
+
+extensions = ['breathe', 'myst_parser', 'sphinx.ext.graphviz']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'shibuya'
+html_theme_options = {
+    "accent_color": "grass",
+}
+html_static_path = ['_static']
+
+# -- Breathe configuration ---------------------------------------------------
+breathe_default_project = "eecs"
