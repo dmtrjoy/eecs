933dd10aa2011adfc9eeec2a164111a05c652d4d
dmtrjoy <137457240+dmtrjoy@users.noreply.github.com>
2025-09-25 00:06:49 -0400

    Set up automatic Doxyfile configuration in readthedocs builds
    Added a function to configure Doxyfile for documentation generation.

--- docs/conf.py
+++ docs/conf.py
@@ -36,8 +36,28 @@ html_static_path = ['_static']
 breathe_default_project = 'eecs'
 
 import subprocess, os
+import subprocess, os
 
+def configureDoxyfile(input_dir, output_dir):
+    with open('Doxyfile.in', 'r') as file :
+        filedata = file.read()
+
+    filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
+    filedata = filedata.replace('@DOXYGEN_OUTPUT_DIR@', output_dir)
+
+    with open('Doxyfile', 'w') as file:
+        file.write(filedata)
+
+# Check if we're running on Read the Docs' servers
 read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
+breathe_projects = {}
+
 if read_the_docs_build:
-     subprocess.call('pwd', shell=True)
+    input_dir = '../eecs'
+    output_dir = 'build'
+    configureDoxyfile(input_dir, output_dir)
+    subprocess.call('doxygen', shell=True)
+    breathe_projects['eecs'] = output_dir + '/xml'
+
+# ...
